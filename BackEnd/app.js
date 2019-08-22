9dd045033c4652a0983e6fea902ef01799fa7953
Suzanne Alidina <suzannealidina@gmail.com>
2019-08-21 22:28:09 -0400

    adding additional routes

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -14,7 +14,7 @@ var bodyParser=require("body-parser");
 var connection = mysql.createConnection({
               host     : 'localhost',
               user     : 'root',
-              password : '',
+              password : 'password',
               database : 'test'
             });
  
@@ -42,9 +42,14 @@ app.get('/', function(req,res){
   res.sendfile(__dirname + '/public/index.html');
  });//call for main index page
 app.get('/index', routes.index);//call for signup page
-app.get('/home/index', routes.index);
+//app.get('/home/index', routes.index);
+
+app.get('/home', function(req,res){
+  res.sendfile(__dirname + '/public/index.html');
+});
 app.get('/signup', user.signup);//call for signup page
-app.post('/signup', user.signup);//call for signup post 
+app.post('/signup', user.signup);//call for signup post
+app.get('/home/signup', user.signup);//call for signup page from home
 app.get('/login', routes.index);//call for login page
 app.post('/login', user.login);//call for login post
 app.get('/home/dashboard', user.dashboard);//call for dashboard page after login
